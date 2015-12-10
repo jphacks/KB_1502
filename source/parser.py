945354ed8fda13e5c735c471d7358037bdee8f1e
uchihashi_k <uchihashi.kenshi@gmail.com>
2015-12-11 05:28:38 +0900

    ラベルつけた

--- source/parser.py
+++ source/parser.py
@@ -6,55 +6,57 @@ import os
 import sys
 
 file_path = "../training_dataset/nuc/"
+LABELED_DATA_NUM = 1000
 
-if os.path.exists("../training_dataset/sentiment/labeled_text.txt"):
+if os.path.exists("../training_dataset/sentiment/unlabeled_text.txt"):
     print "上書きしちゃうので終了"
     sys.exit()
 
-for doc_index in xrange(1, 100):
+# sentenceの辞書を作る
+sentences = []
+sentence_num = 0
+for doc_index in xrange(1, 101):
     text_file = open(file_path + "data%03d.txt" % doc_index, "r")
     inline = text_file.readline()
 
-    if doc_index == 1:
-        # トレーニングデータを保持するテキスト
-        labeled_text = open("../training_dataset/sentiment/labeled_text.txt", "w")
-        unlabeled_text = open("../training_dataset/sentiment/unlabeled_text.txt", "w")
-    else:
-        unlabeled_text = open("../training_dataset/sentiment/unlabeled_text.txt", "a")
-
-    sentences = []
-    sentence_index = 1
     while inline:
         if not re.search(r"＠", inline):
-
-            # 会話文の取得
             sentence = inline.split("：")[-1]
-            if doc_index == 1:
-                if sentence_index < 101:
-                    labeled_text = open("../training_dataset/sentiment/labeled_text.txt", "a")
-                    print sentence
-
-                    print "保存しますか？(保存する:1, 保存しない:0):"
-                    save_flag = raw_input()
-
-                    if save_flag == "1":
-                        print "ラベルを入力してください:"
-                        label = raw_input()
-
-                        labeled_text.writelines("%s:" % label + sentence)
-                        print "保存しました."
-
-                    labeled_text.close()
-                else:
-                    break
-            else:
-                unlabeled_text.writelines(sentence)
-
             sentences.append(sentence)
-
-            sentence_index += 1
+            sentence_num += 1
         inline = text_file.readline()
 
-    labeled_text.close()
-    unlabeled_text.close()
+print sentence_num
 
+
+index_list = [i for i in xrange(sentence_num)]
+random_index_list = numpy.random.permutation(index_list)
+
+labeled_text = open("../training_dataset/sentiment/labeled_text.txt", "w")
+unlabeled_text = open("../training_dataset/sentiment/unlabeled_text.txt", "w")
+for i, random_index in enumerate(random_index_list):
+    sentence = sentences[random_index]
+    if i < LABELED_DATA_NUM:
+        print ""
+        print sentence
+        print "保存しますか？(保存する:1, 0.5で保存する:0, 保存しない:Enter):"
+        save_flag = raw_input()
+
+        # ラベルあり
+        if save_flag == "1":
+            print "ラベルを入力してください:"
+            label = raw_input()
+
+            labeled_text.writelines("%s:" % label + sentence)
+            print "保存しました."
+
+        # ラベル無し
+        elif save_flag == "0":
+            label = 0.5
+            labeled_text.writelines("%s:" % label + sentence)
+            print "ニュートラルで保存しました."
+    else:
+        unlabeled_text.writelines(sentence)
+
+labeled_text.close()
+unlabeled_text.close()
